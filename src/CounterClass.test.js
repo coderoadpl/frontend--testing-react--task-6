d6a44882ba597f4771beb3a8e7048171cd6a03cb
Mateusz Choma <kontakt@coderoad.pl>
2021-11-20 06:59:19 +0100

    First `CounterClass` test added

--- src/CounterClass.test.js
+++ src/CounterClass.test.js
@@ -1,5 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import CounterClass from './CounterClass'
+
 describe('CounterClass test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true)
+  it('should render valid initial number', () => {
+    const div = document.createElement('div')
+
+    ReactDOM.render(<CounterClass />, div)
+
+    const h1 = div.querySelector('h1')
+
+    expect(h1.textContent).toBe('0')
+
+    ReactDOM.unmountComponentAtNode(div)
   })
 })
