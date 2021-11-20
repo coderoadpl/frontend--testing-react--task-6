d0f072a62ef67b6a5bcb85adf87d61cfedb567ca
Mateusz Choma <kontakt@coderoad.pl>
2021-11-20 07:02:23 +0100

    `CounterClass` tests with JSDOM

--- src/CounterClass.test.js
+++ src/CounterClass.test.js
@@ -15,4 +15,19 @@ describe('CounterClass test', () => {
 
     ReactDOM.unmountComponentAtNode(div)
   })
+
+  it('should render valid number after button click', () => {
+    const div = document.createElement('div')
+
+    ReactDOM.render(<CounterClass />, div)
+
+    const h1 = div.querySelector('h1')
+    const button = div.querySelector('button')
+
+    button.click()
+
+    expect(h1.textContent).toBe('1')
+
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
